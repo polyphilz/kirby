2ca528074fff40de8f3cb2194f727e7add211b51
Rohan Bansal <rohanarunbansal@gmail.com>
2022-03-21 19:51:21 -0700

    Update blocked page

--- background.js
+++ background.js
@@ -8,7 +8,7 @@ chrome.tabs.onUpdated.addListener((tabId, changeInfo, tab) => {
   chrome.storage.sync.get("sites", (data) => {
     for (const site of data.sites) {
       if (url.includes(site)) {
-        chrome.tabs.update(tabId, { url: "./blah.html" });
+        chrome.tabs.update(tabId, { url: "./blocked.html" });
       }
     }
   });
