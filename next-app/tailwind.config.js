c34b2b31fe22e5b06f8d6ecfed7f0f42c80dd204
Rohan Bansal <rohanarunbansal@gmail.com>
2022-03-21 21:41:39 -0700

    Add Tailwind support

---
+++ next-app/tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mode: 'jit',
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+  purge: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+}
