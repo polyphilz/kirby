74edc0701134514135696eba29bb7804eb471846
Rohan Bansal <rohanarunbansal@gmail.com>
2022-03-22 20:27:11 -0700

    Add some global fonts/colors, etc.

--- next-app/tailwind.config.js
+++ next-app/tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
-  content: ['./components/**/*.{js,ts,jsx,tsx}', './pages/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        Mukta: ["Mukta", "sans-serif"],
+      },
+    },
+    colors: {
+      "light-pink-1": "#EC9DB8",
+      "dark-pink-1": "#F06F9B",
+    },
   },
   plugins: [],
-}
+};
